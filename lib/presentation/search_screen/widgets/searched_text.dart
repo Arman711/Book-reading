082062724642179f4e6b150756fb70b4ec6f43df
Arman711 <musaelyana11@gmail.com>
2025-01-05 15:32:40 +0400

    icon with flutter gen

--- lib/presentation/search_screen/widgets/searched_text.dart
+++ lib/presentation/search_screen/widgets/searched_text.dart
@@ -1,6 +1,8 @@
+import 'package:book_reading/gen/assets.gen.dart';
 import 'package:book_reading/presentation/core/constants/colors.dart';
 import 'package:book_reading/presentation/core/constants/typography.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SearchedText extends StatelessWidget {
   final String text;
@@ -27,9 +29,13 @@ class SearchedText extends StatelessWidget {
             style: AppTypography.s14w4c30,
           ),
           const Spacer(),
-          const Icon(
-            Icons.cancel_outlined,
-            color: AppColor.a30,
+          SizedBox(
+            child: SvgPicture.asset(
+              Assets.icons.delete,
+              height: 18,
+              width: 18,
+              fit: BoxFit.none,
+            ),
           ),
         ],
       ),
